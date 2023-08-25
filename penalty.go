6cc347cd5ca373247072139646cc9df2a4e17be6
beck <1504068285@qq.com>
2023-08-26 00:38:39 +0800

    add json ouput

--- penalty.go
+++ penalty.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	b "math/big"
+	"net/http"
 	"sort"
 	"strconv"
 	"time"
@@ -16,15 +17,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type APIResponse struct {
+	Code  int         `json:"code"`
+	Level int         `json:"level"`
+	Msg   string      `json:"msg"`
+	Data  interface{} `json:"data"`
+}
+
 func penalty(c *gin.Context) {
 	// 获取查询参数值
 	miner := c.Query("miner")
 	if miner == "" {
-		c.String(400, "please specify a miner")
+		c.JSON(http.StatusBadRequest, APIResponse{
+			Code: http.StatusBadRequest,
+			Msg:  "please specify a miner",
+		})
+		return
 	}
 	mid, err := address.NewFromString(miner)
 	if err != nil {
-		c.String(400, err.Error())
+		c.JSON(http.StatusBadRequest, APIResponse{
+			Code: http.StatusBadRequest,
+			Msg:  err.Error(),
+		})
+		return
 	}
 
 	allSectors, _ := strconv.ParseBool(c.DefaultQuery("all", "0"))
@@ -32,16 +48,30 @@ func penalty(c *gin.Context) {
 	// 往后/往前 推多少天
 	offset, _ := strconv.ParseInt(c.DefaultQuery("offset", "0"), 10, 64)
 
-	data, err := Compute(mid, allSectors, abi.ChainEpoch(offset*2880))
+	jsonOut, _ := strconv.ParseBool(c.DefaultQuery("json", "0"))
+
+	data, err := Compute(mid, allSectors, abi.ChainEpoch(offset*2880), jsonOut)
 	if err != nil {
 		log.Printf("%v\n", err)
-		c.String(500, err.Error())
+		c.JSON(http.StatusInternalServerError, APIResponse{
+			Code: http.StatusInternalServerError,
+			Msg:  err.Error(),
+		})
+		return
 	}
-	c.String(200, data)
+	if jsonOut {
+		c.JSON(http.StatusOK, APIResponse{
+			Code: http.StatusOK,
+			Msg:  "OK",
+			Data: data,
+		})
+	} else {
+		c.String(200, data.(string))
+	}
 
 }
 
-func Compute(mid address.Address, allSectors bool, offset abi.ChainEpoch) (string, error) {
+func Compute(mid address.Address, allSectors bool, offset abi.ChainEpoch, jsonOut bool) (interface{}, error) {
 
 	type daliyData struct {
 		penalty abi.TokenAmount
@@ -125,6 +155,15 @@ func Compute(mid address.Address, allSectors bool, offset abi.ChainEpoch) (strin
 		return sortedKeys[i] < sortedKeys[j]
 	})
 
+	type dayData struct {
+		Date        string          `json:"date"`
+		Mid         address.Address `json:"mid"`
+		Sectors_sum int             `json:"sectors_sum"`
+		Power       float64         `json:"power"`
+		Pledge      string          `json:"pledge"`
+		Penalty     string          `json:"penalty"`
+	}
+	dayDatas := make([]*dayData, 0)
 	outData := ""
 	// 表头
 	outData += fmt.Sprintln("date,mid,sectors_sum,power(TiB),pledge,penalty")
@@ -141,8 +180,16 @@ func Compute(mid address.Address, allSectors bool, offset abi.ChainEpoch) (strin
 		for _, v := range data.info {
 			daliyPledge = big.Add(daliyPledge, v)
 		}
-
-		outData += fmt.Sprintf("%v,%v,%v,%v,%v,%v\n", date, mid, seLen, float64(minerInfo.SectorSize*abi.SectorSize(seLen))/(1<<40), new(b.Rat).SetFrac(daliyPledge.Int, b.NewInt(1e18)).FloatString(10), new(b.Rat).SetFrac(data.penalty.Int, b.NewInt(1e18)).FloatString(10))
+		structData := &dayData{
+			Date:        date,
+			Mid:         mid,
+			Sectors_sum: seLen,
+			Power:       float64(minerInfo.SectorSize*abi.SectorSize(seLen)) / (1 << 40),
+			Pledge:      new(b.Rat).SetFrac(daliyPledge.Int, b.NewInt(1e18)).FloatString(10),
+			Penalty:     new(b.Rat).SetFrac(data.penalty.Int, b.NewInt(1e18)).FloatString(10),
+		}
+		dayDatas = append(dayDatas, structData)
+		outData += fmt.Sprintf("%v,%v,%v,%v,%v,%v\n", date, mid, seLen, structData.Power, structData.Pledge, structData.Penalty)
 
 		sectors_sum += seLen
 		power += minerInfo.SectorSize * abi.SectorSize(seLen)
@@ -152,6 +199,9 @@ func Compute(mid address.Address, allSectors bool, offset abi.ChainEpoch) (strin
 	// 汇总数据
 	outData += fmt.Sprintf(",,%v,%v,%v,%v\n", sectors_sum, float64(power)/(1<<40), new(b.Rat).SetFrac(pledge.Int, b.NewInt(1e18)).FloatString(10), new(b.Rat).SetFrac(penalty.Int, b.NewInt(1e18)).FloatString(10))
 
+	if jsonOut {
+		return dayDatas, nil
+	}
 	return outData, nil
 }
 
